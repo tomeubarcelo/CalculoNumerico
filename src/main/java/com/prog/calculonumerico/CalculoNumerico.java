1aee22d622cb5b704a44b9b44f925618577ec433
Tomeu Barcelo <tomeu.95@gmail.com>
2021-02-18 09:43:49 +0100

    funciona cambio icono app

--- src/main/java/com/prog/calculonumerico/CalculoNumerico.java
+++ src/main/java/com/prog/calculonumerico/CalculoNumerico.java
@@ -33,7 +33,8 @@ public class CalculoNumerico extends javax.swing.JFrame {
         
         this.setTitle("Ejercicio 2 - Cálculo numérico");
         //Image icon = new ImageIcon(getClass().getResource("/../resources/mallorca.png")).getImage();
-        //this.setIconImage(icon);        
+        //this.setIconImage(icon);   
+        this.setIconImage(new javax.swing.ImageIcon("C:\\Users\\tomeu\\Downloads\\boton-de-play.png").getImage());
         label_valor_minim.setVisible(false);
         label_valor_max.setVisible(false);
         btn_reset.setVisible(false);
